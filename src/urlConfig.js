602abd9541f2cf6bbe6e41500bdbf74b6cc5b348
LQuangTien <tienkg5554@gmail.com>
2022-05-28 17:32:05 +0700

    Update domain

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,16 +1,16 @@
 export const api =
   process.env.REACT_APP_MODE === "production"
     ? "https://quangtien-ecommerce-be.herokuapp.com/"
-    : "http://http://52.0.166.104/:3500/";
+    : "http://http://52.0.166.104:3500/";
 export const domain =
   process.env.REACT_APP_MODE === "production"
     ? "https://quangtien-ecommerce-be.herokuapp.com/admin"
-    : "http://http://52.0.166.104/:3500";
+    : "http://http://52.0.166.104:3500";
 
 export const clientDomain =
   process.env.REACT_APP_MODE === "production"
     ? "https://ecommerce-client-teal.vercel.app/"
-    : "http://http://52.0.166.104/:3000/";
+    : "http://http://52.0.166.104:3000/";
 export const generatePictureUrl = (filename) => {
   return filename;
 };
